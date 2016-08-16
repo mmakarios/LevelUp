40d9ce3fc7d9d409962f4da1f6585bf52490ef21
mmakarios <michel_makarios@hotmail.com>
2016-08-16 14:18:19 -0300

    Force text, chase behavior

---
+++ Assets/Scripts/Chaser.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chaser : MonoBehaviour {
+
+	public float speed;
+	public Transform targetTransform;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 displacement = targetTransform.position - transform.position;
+		if (displacement.magnitude > 1.5)
+		{
+			Vector3 direction = displacement.normalized;
+			Vector3 velocity = direction * speed;
+			transform.Translate(velocity * Time.deltaTime);
+			
+		}
+	}
+}
