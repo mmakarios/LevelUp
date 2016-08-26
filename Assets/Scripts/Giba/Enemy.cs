76cbf9b12a8f0c40efcd4ef44f14c20b72680cdf
Guilherme <guibasconti@gmail.com>
2016-08-25 23:52:08 -0300

    Ep 4

---
+++ Assets/Scripts/Giba/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Enemy : LivingEntity
+{
+
+    NavMeshAgent pathFinder;
+    Transform target;
+
+    // Use this for initialization
+    protected override void Start() {
+
+        base.Start();
+
+        pathFinder = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        StartCoroutine(UpdatePath());
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+
+
+    }
+
+    IEnumerator UpdatePath()
+    {
+        float refreshRate = .25f;
+
+        while(target != null)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
+            if (!dead){
+                pathFinder.SetDestination(targetPosition);
+            }
+            yield return new WaitForSeconds(refreshRate);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Giba/GunController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunController : MonoBehaviour {
+
+    Gun equippedGun;
+    public Transform weaponHold;
+    public Gun startingGun;
+
+    public void Start()
+    {
+
+        if (startingGun != null)
+        {
+            EquipGun(startingGun);
+        }
+
+    }
+
+	public void EquipGun(Gun gunToEquip)
+    {
+
+        if (equippedGun != null)
+        {
+            Destroy(gunToEquip.gameObject);
+        }
+        equippedGun = gunToEquip;
+
+        equippedGun = Instantiate(gunToEquip, weaponHold.position, weaponHold.rotation) as Gun;
+        equippedGun.transform.parent = weaponHold;
+
+    }
+
+    public void Shoot()
+    {
+
+        if(equippedGun != null)
+        {
+
+            equippedGun.Shoot();
+
+        }
+
+    }
+
+}
