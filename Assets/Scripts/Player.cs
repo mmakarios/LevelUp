2baa5a87a01ed224ae5c3e7497d300d6bc81b078
Michel <michel.makarios@gmail.com>
2016-08-26 02:16:32 -0300

    Player olhando pra camera

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,20 +3,32 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	public float speed;
+	public float moveSpeed;
 
-	// Use this for initialization
+	Camera viewCamera;
+	PlayerController controller;
+
 	void Start () {
-	
+		controller = FindObjectOfType<PlayerController>();
+		viewCamera = Camera.main;
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-		Vector3 direction = input.normalized;
-		Vector3 velocity = direction * speed;
-		Vector3 moveAmount = velocity * Time.deltaTime;
-		transform.Translate(moveAmount);
+		Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+		Vector3 moveDirection = moveInput.normalized;
+		Vector3 moveVelocity = moveDirection * moveSpeed;
+		// Vector3 moveAmount = moveVelocity * Time.deltaTime;
+		// transform.Translate(moveAmount);
+		controller.Move(moveVelocity);
 
+		Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+		Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+		float rayDistance;
+
+		if (groundPlane.Raycast(ray, out rayDistance)) {
+			Vector3 point = ray.GetPoint(rayDistance);
+			Debug.DrawLine(ray.origin, point, Color.red);
+			controller.LookAt(point);
+		}
 	}
 }
